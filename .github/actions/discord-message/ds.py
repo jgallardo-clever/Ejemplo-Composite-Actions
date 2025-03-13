5b864b75b1b1e239b1b2b1d964c440cd74be05ad
Jorge Gallardo <jorgito899@gmail.com>
2025-03-13 11:26:47 -0300

    Se agrega el composite action de Discord con Webhook

---
+++ .github/actions/discord-message/ds.py
@@ -0,0 +1,11 @@
+# Action que envía un mensaje a un canal de Discord, indicando el nombre del usuario que ha ejecutado el workflow.
+
+from discord_webhook import DiscordWebhook
+import sys
+
+# Recibir por parámetro el nombre del usuario que ha ejecutado el workflow
+url = sys.argv[1]
+user = sys.argv[2]
+
+webhook = DiscordWebhook(url="{url}", content="{user}")
+response = webhook.execute()
